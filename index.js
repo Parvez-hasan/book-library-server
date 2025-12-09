1d2ebd1d88ad3e5d7e907b3a2e577d9825c84f69
Parvez-hasan <parvez7788@gmail.com>
2025-12-09 21:49:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 3000
+
+// middleware
+app.use(express.json());
+app.use(cors())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@project-1.zd08b5r.mongodb.net/?appName=project-1`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const db = client.db('book_library_db')
+    const booksCollection = db.collection('books')
+
+    //books api
+    app.post('/books', async(req,res) => {
+       const book = req.body;
+       const result = booksCollection.insertOne(book);
+       res.send(result)
+    })
+
+    app.get('/books', async(req,res) => {
+
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+   // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('book laybaray runing')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
